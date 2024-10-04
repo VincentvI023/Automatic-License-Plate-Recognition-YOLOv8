b82476ba994be475fabbac7946903940c977fd29
Vincent van Ingen <info@vincentvaningen.nl>
2024-10-04 22:22:25 +0200

    Everything is writing now to CSV, for efficiency and no corruption

--- main.py
+++ main.py
@@ -7,59 +7,43 @@ from sort.sort import *
 from util import get_car, read_license_plate, load_last_frame, save_frame_number
 import os
 import csv
-import openpyxl
+import csv
 import numpy as np
 from tqdm import tqdm
 
+# Change the following variables to the correct paths
 plate_folder = '/plates'
+csv_file_path_licence_plates = './output/3-10-2024-ochtend-licence.csv'
+csv_file_path_counter = './output/3-10-2024-ochtend-counts.csv'
+start_time_str = "07:38"  # Starttijd in het formaat "HH:MM"
+cap = cv2.VideoCapture('./videos/3-10-2024-ochtend.mov')
 
 
 results = {}
-
 mot_tracker = Sort()
 
 # load models
 coco_model = YOLO('yolov8n.pt')
 license_plate_detector = YOLO('license_plate_detector.pt')
 
-# load video
-cap = cv2.VideoCapture('./videos/3-10-2024-ochtend.MOV')
 start_frame = load_last_frame()
-
-
-# Starttijd invoeren (bijvoorbeeld: de tijd waarop de video is begonnen)
-start_time_str = "07:38"  # Starttijd in het formaat "HH:MM"
 start_time = datetime.strptime(start_time_str, "%H:%M")  # Omzetten naar datetime object
-
-# Clear all rows except the first one in the CSV file
-# csv_file_path = './test.csv'
-# with open(csv_file_path, 'r') as file:
-#     reader = csv.reader(file)
-#     header = next(reader)
-
-# with open(csv_file_path, 'w', newline='') as file:
-#     writer = csv.writer(file)
-#     writer.writerow(header)
 
 vehicles = [2, 3, 5, 7]
 vehicle_info = [
-    {"id": 2, "name": "car", "count": 2947},
-    {"id": 3, "name": "motorcycle", "count": 112},
-    {"id": 5, "name": "bus", "count": 138},
-    {"id": 7, "name": "truck", "count": 246}
+    {"id": 2, "name": "car", "count": 0},
+    {"id": 3, "name": "motorcycle", "count": 0},
+    {"id": 5, "name": "bus", "count": 0},
+    {"id": 7, "name": "truck", "count": 0}
 ]
 
 unique_vehicle_ids = set()
 track_id_to_vehicle_type = {}
 
 # read frames
-frame_nmr = 163078
+frame_nmr = load_last_frame()
 ret = True
-
-# Get total number of frames
 total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
-# Initialize progress bar
 progress_bar = tqdm(total=total_frames, desc="Processing frames", initial=frame_nmr + 1)
 
 while ret:
@@ -67,6 +51,7 @@ while ret:
     ret, frame = cap.read()
     progress_bar.update(1)
     frame_nmr += 1
+    save_frame_number(frame_nmr)
     ret, frame = cap.read()
     if ret:
         results[frame_nmr] = {}
@@ -99,45 +84,39 @@ while ret:
                     
                     # If get_car returns a valid track_id (not -1), check if it's unique
                     if car_track_id != -1 and car_track_id not in unique_vehicle_ids:
-                        # Add the unique track_id to the set
                         unique_vehicle_ids.add(car_track_id)
-                        
-                        # Record the vehicle class (car, bus, etc.)
                         track_id_to_vehicle_type[car_track_id] = int(class_id)
                         
                         # Increment the count for the corresponding vehicle class
                         for vehicle in vehicle_info:
                             if vehicle["id"] == int(class_id):
-                                vehicle["count"] += 1  # Increment count for this vehicle type
-                                # Write the updated vehicle counts to the Excel sheet
-                                excel_file_path = './vehicle_counts.xlsx'
+                                # Check if the CSV file exists
+                                file_exists = os.path.isfile(csv_file_path_counter)
 
-                                # Load the workbook and select the active sheet
-                                if os.path.exists(excel_file_path):
-                                    workbook = openpyxl.load_workbook(excel_file_path)
-                                    sheet = workbook.active
+                                # Read existing data from the CSV file
+                                if file_exists:
+                                    with open(csv_file_path_counter, 'r') as csvfile:
+                                        reader = csv.reader(csvfile)
+                                        existing_data = list(reader)
                                 else:
-                                    workbook = openpyxl.Workbook()
-                                    sheet = workbook.active
-                                    sheet.append(["Vehicle Type", "Count"])
+                                    existing_data = [["Voertuig type", "Aantal"]]
 
-                                # Find the row corresponding to the vehicle type and update the count
-                                for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row, min_col=1, max_col=2):
-                                    if row[0].value == vehicle["name"]:
-                                        row[1].value = vehicle["count"]
+                                # Update the count for the vehicle type
+                                for row in existing_data:
+                                    if row[0] == vehicle["name"]:
+                                        vehicle["count"] = int(row[1]) + 1  # Increment count based on CSV value
+                                        row[1] = str(vehicle["count"])
                                         break
                                 else:
                                     # If the vehicle type is not found, append a new row
-                                    sheet.append([vehicle["name"], vehicle["count"]])
+                                    vehicle["count"] = 1
+                                    existing_data.append([vehicle["name"], str(vehicle["count"])])
 
-                                # Save the workbook
-                                workbook.save(excel_file_path)
+                                # Write the updated data back to the CSV file
+                                with open(csv_file_path_counter, 'w', newline='') as csvfile:
+                                    writer = csv.writer(csvfile)
+                                    writer.writerows(existing_data)
                                 break
-
-        # After processing, print the unique vehicle counts
-        print(f"Total unique vehicles: {len(unique_vehicle_ids)}")
-        for vehicle in vehicle_info:
-            print(f"Total {vehicle['name']}s: {vehicle['count']}")
 
         # detect license plates
         license_plates = license_plate_detector(frame)[0]
@@ -188,7 +167,7 @@ while ret:
                 # Select the best variant based on the text score
                 license_plate_text, license_plate_text_score, car_values = max(license_plate_text_variants, key=lambda x: x[1] if x[0] is not None else 0)
                 
-                if license_plate_text is not None and license_plate_text_score > 0.55:
+                if license_plate_text is not None and license_plate_text_score > 0.30:
                     print("--------------------------------------------------")
                     print(license_plate_text, license_plate_text_score, car_values[0], car_values[1])
                     print("--------------------------------------------------")
@@ -205,21 +184,20 @@ while ret:
                     # Format detection time as string
                     formatted_detection_time = detection_time.strftime("%H:%M:%S.%f")[:-3]
 
-                    excel_file_path = './car_values_test_day.xlsx'
+                    # Check if the CSV file exists
+                    file_exists = os.path.isfile(csv_file_path_licence_plates)
 
-                    # Load the workbook and select the active sheet
-                    if os.path.exists(excel_file_path):
-                        workbook = openpyxl.load_workbook(excel_file_path)
-                        sheet = workbook.active
+                    # Open the CSV file in append mode
+                    with open(csv_file_path_licence_plates, 'a', newline='') as csvfile:
+                        writer = csv.writer(csvfile)
 
-                    # Append the new row with car values and actual time
-                    sheet.append([license_plate_text, license_plate_text_score, car_values[0], car_values[1], formatted_detection_time])
-                    workbook.save(excel_file_path)
+                        # If the file doesn't exist, write the header
+                        if not file_exists:
+                            writer.writerow(["Kenteken", "Kenteken Score", "Type voertuig", "Auto merk", "Tijd"])
+
+                        # Write the new row with car values and actual time
+                        writer.writerow([license_plate_text, license_plate_text_score, car_values[0], car_values[1], formatted_detection_time])
                     
-                    # Write results to CSV
-                    # with open(csv_file_path, 'a', newline='') as file:
-                    #     writer = csv.writer(file)
-                    #     writer.writerow([frame_nmr, car_id, xcar1, ycar1, xcar2, ycar2, x1, y1, x2, y2, license_plate_text, score, license_plate_text_score, formatted_detection_time])
                 else:
                     continue      
 print("End of processing")
